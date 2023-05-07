33fac9cba2cce3313db5c84c939d3f0c4d88ec72
xabepa <xabepaa@googlemail.com>
2023-05-07 19:29:39 +0200

    fix some spelling

--- crack_parallel.sh
+++ crack_parallel.sh
@@ -4,46 +4,34 @@ run_crackme() {
 	param=$1
 	k=$2
 
-	# echo "current pw is: $param and param k is $k"
-
 	# Start timer
 	start=$(date +%s.%N)
 
 	# Run crackme with user input parameter and capture its PID
+	# ignore output
 	./crackme $param > /dev/null 2>&1 &
 	pid=$!
 	# echo "PID: $pid"
-
-	# Wait for the crackme process to finish and capture its return value
 	wait $pid
 	ret=$?
 
-	# Stop timer and calculate runtime
-	end=$(date +%s.%N)
-
 	# store runtime as integer in variable
+	end=$(date +%s.%N)
 	runtime=$(echo "($end - $start) * 1000000" | bc | cut -f1 -d'.')
 
-	# Print results
-	# echo "Return value: $ret"
-	# echo "PID: $pid"
-
+	# use derandomizer to get random number
 	random_number=$(./derandomizer $pid ${param:0:1})
-
-	# echo "random number: $random_number"
 	rand5=$((random_number%5))
 
-	# echo "rand % 5 = $rand5"
+	# calculate random waittime
 	wait_time=$((($rand5 * 800000))) # / 1000000))
-
 	# echo "waittime without punishment in s: $wait_time"
 
+	# calculate punishment time, 8 is the worst case and we know that k chars are correct
 	punishment_time=$(((((8-$k))*800000))) 
 
 	# echo "worstcase waittime is: $(($wait_time + $punishment_time))"
 	diff=$(($runtime - ($wait_time + $punishment_time)))
-
-	# echo "diff for $param is: $diff"
 
 	#if time diff is below 0 we hit a right char, cause waited less then worstcase
 	if [[ $diff -lt 0 ]]; then
