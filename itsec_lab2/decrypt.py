7c756ae20f2f69d48432a54948d5c41a678c8b0a
xabepa <xabepaa@googlemail.com>
2023-05-22 12:52:23 +0200

    lab2 first requests

--- itsec_lab2/decrypt.py
+++ itsec_lab2/decrypt.py
@@ -2,6 +2,8 @@ import base64
 from urllib import parse
 import requests
 
+
+
 def main():
     # this is the original secret
     secret = "2QicDQHnGmRuZys0M5JcwCSTeFNXvVm%2FSsG1vaEkIZU1OiGgpLJTdbRO2beA831a0xsatfOy01N38W1RidzrXA%3D%3D"
@@ -13,22 +15,26 @@ def main():
     url_decoded_str = parse.unquote(secret)
 
     # check whether decoded secret is equal to e1
-    print(url_decoded_str == e1)
+    #print(url_decoded_str == e1)
     print(f"unquoted: {url_decoded_str}")
 
     # base64-decode the secret
     hex_chifre_str = base64.b64decode(url_decoded_str)
 
-    hex_str = ""
+    print(bytes_to_hex_str(hex_chifre_str))
 
-    # print the bytes of the decoded secret
-    for i, byte in enumerate(hex_chifre_str):
-        print(f"{i} : {format(byte, '02x')}")
-        hex_str += format(byte, '02x')
+    blocks = get_blocks(bytes_to_hex_str(hex_chifre_str))
 
-    print(hex_str)
+    last_block = str(blocks[-1])
+    print(f"last_block = {last_block}")
+
+    empty_block = bytes(16)
+    empty_block = bytes_to_hex_str(empty_block)
 
     print(send_request(encode_to_b64_url(hex_chifre_str)))
+    
+    print(send_request(encode_to_b64_url(hex_str_to_bytes(empty_block + last_block))).text)
+
 
 
 def encode_to_b64_url(byte_arr: bytes):
@@ -39,12 +45,25 @@ def encode_to_b64_url(byte_arr: bytes):
 def send_request(encoded_str):
     BASE_URL = "http://gruenau2.informatik.hu-berlin.de:8888/store_secret/?secret="
 
-    return requests.get(BASE_URL + encoded_str).status_code
+    return requests.get(BASE_URL + encoded_str)
 
 def get_blocks(hex_str: str):
     blocks = []
-    for i in range(0, len(hex_str), 16):
-        blocks.append(hex_str[i:i+16])
+    for i in range(0, len(hex_str), 32):
+        blocks.append(hex_str[i:i+32])
     return blocks
+
+def bytes_to_hex_str(byte_arr: bytes):
+    hex_str = ""
+
+    # print the bytes of the decoded secret
+    for i, byte in enumerate(byte_arr):
+        # print(f"{i} : {format(byte, '02x')}")
+        hex_str += format(byte, '02x')
+
+    return hex_str
+
+def hex_str_to_bytes(str):
+    return bytes.fromhex(str)
 
 main()
