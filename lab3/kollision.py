a8199af02d6c2a7164535215ff4a8a9b01124127
Kai <kai.werk@gmx.de>
2023-06-19 14:14:23 +0200

    corrected explanation

--- lab3/kollision.py
+++ lab3/kollision.py
@@ -5,8 +5,8 @@ from Crypto.Hash import SHA3_224
 
     # okay okay a,b sind privat keys
     # A, B sind public keys
-    # A = g^a mod p und B = g^b mod p -> private Key wird durch k = g^a*b mod p berechnet
-    # berechnen zwei unterschiedliche private keys K* und K~ mit b* und a~, 
+    # A = g^a mod p und B = g^b mod p -> GEMEINSAMER Key wird durch k = g^a*b mod p berechnet
+    # berechnen zwei unterschiedliche GEMEINSAME keys K* und K~ mit b* und a~, 
     # ABER mit dem jeweils RICHTIGEN und ABGEFANGENEN PUBLIC KEY, denn A = g^a also K~ = a~ * B und K* = b* * A 
     # b* und a~ werden mit dem Hash ihres Privat Keys in zwei Tabellen gespeichert
     # und wir warten darauf das wir eine Hashkollision für zwei Hashes aus den jeweiligen Tabellen finden!
